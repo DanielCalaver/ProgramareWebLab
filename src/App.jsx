5630904ffa0b688d9a2d1d9d102c141375fb6059
Daniel Calaver <daniel.calaver@unitbv.ro>
2026-03-11 02:58:23 +0200

    feature: adauga buton de count

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import Card from './Card';
+import { useState } from 'react';
+
 
 function App() {
+  const projects = [
+  {title: "Proiect 1", description: "Pagina personala cu HTML/CSS" },
+  {title: "Proiect 2", description: "Pagina interactiva cu JavaScript"},
+  {title: "Proiect 3", description: "Dashboard cu React"},
+  {title: "Proiect 4", description: "Proiect interesant"},
+  {title: "Proiect 5", description: "Proiect revolutionar"}
+  ]
+  const [count, setCount] = useState(0);
   return (
     <div>
       <h1>Dashboard</h1>
-      <Card title = "Proiect 1" description="Pagina personala cu HTML/CSS"/>
-      <Card title = "Proiect 2" description="Pagina interactiva cu JavaScript"/>
-      <Card title = "Proiect 3" description="Dashboard cu React"/>
+      {projects.map(function(item, index) {
+        return <Card key={index} title={item.title} description={item.description} />;
+      })}
+      <button onClick={() => setCount(count+1)}>Click</button>
+      <p>Ai apasat de {count} ori</p>
     </div>
   );
 }
